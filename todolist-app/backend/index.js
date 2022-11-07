c4a512ea266cecb0cf4948a4be17dae5887e8b5d
Hazal Yılmaz <hazal.yilmazx@hotmail.com>
2022-11-07 19:30:24 +0300

    first commit

---
+++ todolist-app/backend/index.js
@@ -0,0 +1,14 @@
+const connection = require('./src/configs/database');
+const cors = require('cors');
+const express = require('express');
+const app = express();
+
+connection()
+
+app.use(express.json());
+app.use(cors());
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+    console.log(`Listening on port ${port}...`);
+})
---
+++ todolist-app/backend/src/configs/database.js
@@ -0,0 +1,19 @@
+// Configure MongoDB Databases
+const mongoose = require('mongoose');
+
+module.exports = async () => {
+    try {
+        const connectionParams = {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useUnifiedTopology: true,
+        }
+        await mongoose.connect('mongodb://localhost:27017/todolist-app', () => {
+            connectionParams
+        })
+        console.log('Connected to database');
+    }
+    catch (error) {
+        console.log('Could not connect to database', error);
+    }
+}
